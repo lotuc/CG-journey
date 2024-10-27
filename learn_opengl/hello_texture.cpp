80f7c72834f27f72ebe4c7f8c7a495756ad00802
lotuc <lotu.c@outlook.com>
2024-10-27 11:59:44 +0800

    Learn OpenGL - texture units - apply multiple texture

--- learn_opengl/hello_texture.cpp
+++ learn_opengl/hello_texture.cpp
@@ -16,6 +16,30 @@ void processInput(GLFWwindow *window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
+unsigned int load_texture(const char *texture_file, GLenum format, bool flip) {
+  int width, height, nrChannels;
+
+  if (flip) {
+    stbi_set_flip_vertically_on_load(flip);
+  }
+
+  unsigned char *data = stbi_load(texture_file, &width, &height, &nrChannels, 0);
+
+  unsigned int texture;
+  glGenTextures(1, &texture);
+
+  glBindTexture(GL_TEXTURE_2D, texture);
+
+  if (data) {
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+    glGenerateMipmap(GL_TEXTURE_2D);
+  } else {
+    std::cout << "Failed to load texture" << std::endl;
+  }
+  stbi_image_free(data);
+  return texture;
+}
+
 int main() {
 
   // glfw: initialize and configure
@@ -49,21 +73,8 @@ int main() {
 
   Shader ourShader("learn_opengl/shaders/3.4.shader.vs", "learn_opengl/shaders/3.4.shader.fs");
 
-  int width, height, nrChannels;
-  unsigned char *data = stbi_load("learn_opengl/textures/container.jpg", &width, &height, &nrChannels, 0);
-
-  unsigned int texture;
-  glGenTextures(1, &texture);
-
-  glBindTexture(GL_TEXTURE_2D, texture);
-
-  if (data) {
-    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
-    glGenerateMipmap(GL_TEXTURE_2D);
-  } else {
-    std::cout << "Failed to load texture" << std::endl;
-  }
-  stbi_image_free(data);
+  unsigned int texture1 = load_texture("learn_opengl/textures/container.jpg", GL_RGB, false);
+  unsigned int texture2 = load_texture("learn_opengl/textures/awesomeface.png", GL_RGBA, true);
 
   // Set up vertex data (and buffer(s)) and configure vertex attributes
   // ------------------------------------------------------------------
@@ -108,20 +119,28 @@ int main() {
 
   glBindBuffer(GL_ARRAY_BUFFER, 0);
 
+  ourShader.use();
+
+  glActiveTexture(GL_TEXTURE0); // default behavior
+  glBindTexture(GL_TEXTURE_2D, texture1);
+
+  glActiveTexture(GL_TEXTURE1);
+  glBindTexture(GL_TEXTURE_2D, texture2);
+
+  ourShader.setInt("texture1", 0);
+  ourShader.setInt("texture2", 1);
+
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
 
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    ourShader.use();
-
     // update the uniform color
     float timeValue = glfwGetTime();
     float greenValue = sin(timeValue) / 2.0f + 0.5f;
     ourShader.setFloat("ourColor", 0.0f, greenValue, 0.0f, 1.0f);
 
-    glBindTexture(GL_TEXTURE_2D, texture);
     glBindVertexArray(VAO);
     glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
